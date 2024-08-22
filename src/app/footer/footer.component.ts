8838ff29707432dd95b6a5e14c06ff2bbab7df30
stukutra <GS>
2024-08-22 16:46:55 +0200

    update all

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  public currentYear: number;
 
+  constructor() {
+    this.currentYear = new Date().getFullYear(); // Ottieni l'anno corrente dinamicamente
+  }
 }
