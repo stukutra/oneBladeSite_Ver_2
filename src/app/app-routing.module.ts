104565da68375b3be6108eabd8b1e7c02a3c8432
stukutra <GS>
2024-08-28 20:46:02 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, data: { animation: 'Home' } },
-  { path: 'about', component: AboutComponent, data: { animation: 'About' } }
+  { path: '', component: HomeComponent, data: { animation: 'Home' } }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
 
-import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
-import { AppContactAziendeComponent } from './app-contact-aziende/app-contact-aziende.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { CircularImageTextComponent } from './circular-image-text/circular-image-text.component';
 import { CompanyDescriptionComponent } from './company-description/company-description.component';
 import { CompanyLogosComponent } from './company-logos/company-logos.component';
 import { ContactFormAziendaComponent } from './contact-form-azienda/contact-form-azienda.component';
@@ -18,27 +15,20 @@ import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ImageCarouselComponent } from './image-carousel/image-carousel.component';
 import { ParallaxComponent } from './parallax/parallax.component';
-import { RecruitmentComponent } from './recruitment/recruitment.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { YourTeamComponent } from './your-team/your-team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent,
     TopBarComponent,
     FooterComponent,
     CompanyLogosComponent,
     CompanyDescriptionComponent,
     ContactFormComponent,
-    RecruitmentComponent,
-    YourTeamComponent,
     ImageCarouselComponent,
     ParallaxComponent,
-    CircularImageTextComponent,
     FullBackgroundComponent,
-    AppContactAziendeComponent,
     ContactFormAziendaComponent
   ],
   imports: [
