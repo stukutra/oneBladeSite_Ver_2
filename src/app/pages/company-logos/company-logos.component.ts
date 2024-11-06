48aa6bd70c0d4eca06073cc269b53041c315459a
stukutra <GS>
2024-11-06 18:26:03 +0100

    Update Logji responsive

--- src/app/pages/company-logos/company-logos.component.ts
+++ src/app/pages/company-logos/company-logos.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-company-logos',
   templateUrl: './company-logos.component.html',
   styleUrls: ['./company-logos.component.scss']
 })
-export class CompanyLogosComponent {
+export class CompanyLogosComponent implements OnInit {
   logos = [
     { src: 'assets/loghiCompany/BNL.png', alt: 'Logo 1' },
     { src: 'assets/loghiCompany/BVTech.png', alt: 'Logo 2' },
@@ -18,7 +18,34 @@ export class CompanyLogosComponent {
     { src: 'assets/loghiCompany/PosteItaliane.png', alt: 'Logo 9' },
     { src: 'assets/loghiCompany/Reply.png', alt: 'Logo 10' },
     { src: 'assets/loghiCompany/telecom.png', alt: 'Logo 11' },
-    { src: 'assets/loghiCompany/ThalesAlenia.png', alt: 'Logo 12' },
-    // Add more logos here
+    { src: 'assets/loghiCompany/ThalesAlenia.png', alt: 'Logo 12' }
   ];
+
+  logosPerSlide: number = 4;
+
+  ngOnInit(): void {
+    // Imposta il valore iniziale per i loghi per slide
+    this.setLogosPerSlide();
+  }
+
+  getSlides(): any[][] {
+    const slides = [];
+    for (let i = 0; i < this.logos.length; i += this.logosPerSlide) {
+      slides.push(this.logos.slice(i, i + this.logosPerSlide));
+    }
+    return slides;
+  }
+
+  private setLogosPerSlide() {
+    const width = window.innerWidth;
+    if (width >= 1200) {
+      this.logosPerSlide = 4;
+    } else if (width >= 992) {
+      this.logosPerSlide = 3;
+    } else if (width >= 768) {
+      this.logosPerSlide = 2;
+    } else {
+      this.logosPerSlide = 1;
+    }
+  }
 }
