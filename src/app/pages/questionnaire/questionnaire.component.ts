4db219636a210253dd2d017e17d6f7d4254c2b0d
stukutra <stukutra@gmail.com>
2024-11-12 21:10:31 +0100

    Update ver 1 questionario

--- src/app/pages/questionnaire/questionnaire.component.ts
+++ src/app/pages/questionnaire/questionnaire.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
-
 interface Question {
   question: string;
   options: string[];
@@ -22,26 +21,46 @@ export class QuestionnaireComponent implements OnInit {
   reportContent: string = '';
   userEmail: string = '';
   currentQuestionIndex: number = 0;
-  
+
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.http.get<Question[]>('/assets/questions.json').subscribe(data => {
-      this.questions = data;
-      this.displayedQuestions = this.questions.slice(0, 5);  // Limita a 5 domande
+      this.questions = this.shuffle(data);
+      this.displayedQuestions = this.questions.slice(0, 5);
+      this.updateProgress();
     });
+  }
+
+  shuffle(array: Question[]): Question[] {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
   }
 
   nextQuestion(): void {
     if (this.currentQuestionIndex < this.displayedQuestions.length - 1) {
       this.currentQuestionIndex++;
+      this.updateProgress();
     } else {
-      this.onSubmit();  // Quando arriviamo alla fine, invia il questionario
+      this.onSubmit();
     }
+  }
+
+  updateProgress(): void {
+    const completedSteps = this.currentQuestionIndex;
+    document.documentElement.style.setProperty('--completed-steps', completedSteps.toString());
+  }
+
+  isCompleted(index: number): boolean {
+    return index < this.currentQuestionIndex || (index === this.currentQuestionIndex && this.reportGenerated);
   }
 
   onSubmit(): void {
     this.reportGenerated = true;
+    this.updateProgress();
     this.generateReport();
   }
 
@@ -57,3 +76,6 @@ export class QuestionnaireComponent implements OnInit {
     alert(`Report inviato a ${this.userEmail}`);
   }
 }
+
+
+
