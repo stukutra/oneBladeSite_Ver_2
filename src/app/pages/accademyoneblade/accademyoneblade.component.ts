faa1f112c7ccd5952bb44e1cab5ed769999797e3
Guido Salzano <stukutra@gmail.com>
2025-01-27 18:50:53 +0100

    add accademy

---
+++ src/app/pages/accademyoneblade/accademyoneblade.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accademyoneblade',
+  templateUrl: './accademyoneblade.component.html',
+  styleUrls: ['./accademyoneblade.component.css']
+})
+export class AccademyonebladeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
