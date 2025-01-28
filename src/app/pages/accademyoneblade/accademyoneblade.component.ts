b7af56c3ccf6a4c9a3b46b67b3b663e30a759449
Guido Salzano <stukutra@gmail.com>
2025-01-28 20:36:10 +0100

    update scss

--- src/app/pages/accademyoneblade/accademyoneblade.component.ts
+++ src/app/pages/accademyoneblade/accademyoneblade.component.ts
@@ -9,18 +9,27 @@ import { CoursesService } from 'src/app/service/Courses.service';
 })
 export class AccademyonebladeComponent implements OnInit {
 
-  constructor(private courseService: CoursesService) { }
-  categories: any[] = [];
+  constructor(private coursesService: CoursesService) { }
+  categories: any[] = []; // Tutte le categorie originali
+  filteredCategories: any[] = []; // Categorie filtrate
+  selectedCategory: string = 'all'; // Valore di default
+
   
-  ngOnInit() {
-    this.courseService.getCourses().subscribe(
-      (data) => {
-        console.log(data.categories);
-        this.categories = data.categories;
-      },
-      (error) => {
-        console.error('Errore durante il caricamento dei dati:', error);
+  ngOnInit(): void {
+    this.coursesService.getCourses().subscribe(data => {
+      this.categories = data.categories;
+
+      if (this.categories.length > 0) {
+        this.selectedCategory = this.categories[0].title; // Imposta la prima categoria
+        this.filterCourses(); // Filtra automaticamente
       }
+    });
+  }
+
+  // Metodo per filtrare le categorie
+  filterCourses(): void {
+    this.filteredCategories = this.categories.filter(
+      category => category.title === this.selectedCategory
     );
   }
 }
