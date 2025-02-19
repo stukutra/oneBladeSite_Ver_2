b6e64ce8dd0e45dd5c2d63b51027106d55c88d4a
Guido Salzano <stukutra@gmail.com>
2025-02-19 17:56:47 +0100

    update

--- src/app/pages/company/companyAboutUs/companyAboutUs.component.ts
+++ src/app/pages/company/companyAboutUs/companyAboutUs.component.ts
@@ -8,17 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class CompanyAboutUsComponent implements OnInit {
 
   constructor() { }
-  data = [
-    { name: 'Fullstack', value: 95 },
-    { name: 'Mobile (IOS, React Native,Android', value: 80 },
-    { name: 'DOT NET', value: 60 },
-    { name: 'Angular', value: 90 },
-    { name: 'Python', value: 40 },
-    { name: 'Data Engineering', value: 60 },
-    { name: 'QA', value: 30 },
-    { name: 'React', value: 50 },
-    { name: 'UI/UX', value: 75 }
-];
+  dataGroups = [
+    {
+      category: 'Sviluppo Web',
+      technologies: [
+        { name: 'Fullstack', value: 95 },
+        { name: 'Angular', value: 90 },
+        { name: 'React', value: 50 }
+      ]
+    },
+    {
+      category: 'Mobile Development',
+      technologies: [
+        { name: 'Mobile (iOS, React Native, Android)', value: 80 },
+        { name: 'IOS', value: 60 },
+        { name: 'React Native', value: 50 }
+      ]
+    },
+    {
+      category: 'Backend & Cloud',
+      technologies: [
+        { name: 'DOT NET', value: 60 },
+        { name: 'Python', value: 40 },
+        { name: 'Data Engineering', value: 60 }
+      ]
+    },
+    {
+      category: 'Quality Assurance & UI/UX',
+      technologies: [
+        { name: 'QA', value: 30 },
+        { name: 'UI/UX', value: 75 }
+      ]
+    }
+  ];
 
   ngOnInit() {
   }
