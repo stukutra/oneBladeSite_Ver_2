f5096d7d8230fa4bc60b3604b0b1190615ba1288
Guido Salzano <stukutra@gmail.com>
2025-02-19 17:50:12 +0100

    update progress bar

--- src/app/pages/company/companyAboutUs/companyAboutUs.component.ts
+++ src/app/pages/company/companyAboutUs/companyAboutUs.component.ts
@@ -8,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class CompanyAboutUsComponent implements OnInit {
 
   constructor() { }
+  data = [
+    { name: 'Fullstack', value: 95 },
+    { name: 'Mobile (IOS, React Native,Android', value: 80 },
+    { name: 'DOT NET', value: 60 },
+    { name: 'Angular', value: 90 },
+    { name: 'Python', value: 40 },
+    { name: 'Data Engineering', value: 60 },
+    { name: 'QA', value: 30 },
+    { name: 'React', value: 50 },
+    { name: 'UI/UX', value: 75 }
+];
 
   ngOnInit() {
   }
