6c8f9189e7767becac99ee141a7e1eee1164189d
Guido Salzano <stukutra@gmail.com>
2025-02-24 20:02:00 +0100

    update translate guides

--- src/app/service/guide.service.ts
+++ src/app/service/guide.service.ts
@@ -3,6 +3,7 @@ import { Observable, map } from 'rxjs';
 import { Guide } from '../models/guide.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,12 @@ import { Injectable } from '@angular/core';
 export class GuideService {
   private jsonUrl = 'assets/data/guides.json';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private translate: TranslateService) { }
 
   getGuides(): Observable<Guide[]> {
-    return this.http.get<{ guides: Guide[] }>(this.jsonUrl).pipe(
+    const lang = this.translate.currentLang || this.translate.defaultLang;
+    const url = `assets/data/guides_${lang}.json`;
+    return this.http.get<{ guides: Guide[] }>(url).pipe(
       map((response: { guides: any; }) => response.guides)
     );
   }
@@ -23,7 +26,7 @@ export class GuideService {
       map(guides => guides.find(guide => this.formatTitle(guide.title) === title))
     );
   }
-  
+
   private formatTitle(title: string): string {
     return title.toLowerCase().replace(/\s+/g, '-');
   }
