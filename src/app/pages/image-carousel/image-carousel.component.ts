1c243f9eef792e74d5b7e988735c6f4cbab67e43
Guido Salzano <stukutra@gmail.com>
2025-02-25 13:52:23 +0100

    update slide top carosel

--- src/app/pages/image-carousel/image-carousel.component.ts
+++ src/app/pages/image-carousel/image-carousel.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-image-carousel',
   templateUrl: './image-carousel.component.html',
   styleUrls: ['./image-carousel.component.scss']
 })
-export class ImageCarouselComponent {
+export class ImageCarouselComponent implements OnInit {
+  currentSlide = 0;
+  slides = [
+    { title: 'CAROUSEL.SLIDE_1.TITLE', subtitle: 'CAROUSEL.SLIDE_1.SUBTITLE', image: './assets/slide/Slide_1.jpg', alt: 'Description of Slide 1' },
+    { title: 'CAROUSEL.SLIDE_2.TITLE', subtitle: 'CAROUSEL.SLIDE_2.SUBTITLE', image: './assets/slide/Slide_1.jpg', alt: 'Description of Slide 2' },
+    { title: 'CAROUSEL.SLIDE_3.TITLE', subtitle: 'CAROUSEL.SLIDE_3.SUBTITLE', image: './assets/slide/Slide_1.jpg', alt: 'Description of Slide 3' }
+  ];
 
+  ngOnInit() {
+    this.startCarousel();
+  }
+
+  startCarousel() {
+    setInterval(() => {
+      this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+    }, 3000); // Change slide every 3 seconds
+  }
 }
