d30b72cb64f03e7a0cfc6569337e547a36e16f2c
Guido Salzano <stukutra@gmail.com>
2025-02-26 11:08:25 +0100

    update

--- src/app/pages/image-carousel/image-carousel.component.ts
+++ src/app/pages/image-carousel/image-carousel.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CoursesService } from '../../service/Courses.service';
+
 @Component({
   selector: 'app-image-carousel',
   templateUrl: './image-carousel.component.html',
@@ -13,13 +15,31 @@ export class ImageCarouselComponent implements OnInit {
     { title: 'CAROUSEL.SLIDE_3.TITLE', subtitle: 'CAROUSEL.SLIDE_3.SUBTITLE', image: './assets/slide/Slide_1.jpg', alt: 'Description of Slide 3' }
   ];
 
+  constructor(private coursesService: CoursesService) { }
+
   ngOnInit() {
     this.startCarousel();
+    this.loadCourseSlides();
   }
 
   startCarousel() {
     setInterval(() => {
       this.currentSlide = (this.currentSlide + 1) % this.slides.length;
     }, 3000); // Change slide every 3 seconds
+  }
+
+  loadCourseSlides() {
+    this.coursesService.getCoursesActive().subscribe((data: any) => {
+      data.forEach((category: any) => {
+        category.courses.forEach((course: any) => {
+          this.slides.push({
+            title: course.title,
+            subtitle: course.nature,
+            image: './assets/slide/Slide_1.jpg', // Use a default image or course-specific image if available
+            alt: `Description of ${course.title}`
+          });
+        });
+      });
+    });
   }
 }
