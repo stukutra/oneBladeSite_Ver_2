71007214ce0ddaecf96d38e9c89a6cfcbbc5f2a5
Guido Salzano <stukutra@gmail.com>
2025-02-27 21:13:45 +0100

    update

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,6 +1,7 @@
 import { AuthService } from 'src/app/service/auth.service';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ReCaptchaV3Service } from 'ngx-captcha';
 
 @Component({
     selector: 'app-register',
@@ -13,18 +14,59 @@ export class RegisterComponent {
         cognome: '',
         email: '',
         cellulare: '',
-        password: ''
+        password: '',
+        confirmPassword: ''
     };
+    captchaResponse: string = '';
+    passwordComplexityMessage: string = '';
+    passwordHasUppercase: boolean = false;
+    passwordHasLowercase: boolean = false;
+    passwordHasNumber: boolean = false;
+    passwordHasSpecialChar: boolean = false;
+    passwordHasMinLength: boolean = false;
+    passwordFocused: boolean = false;
 
-    constructor(private authService: AuthService, private router: Router) { }
+    constructor(private authService: AuthService, private router: Router, private reCaptchaV3Service: ReCaptchaV3Service) { }
+
+    handleSuccess(token: any) {
+        this.captchaResponse = token as string;
+    }
+
+    checkPasswordComplexity() {
+        const password = this.user.password;
+        this.passwordHasUppercase = /[A-Z]/.test(password);
+        this.passwordHasLowercase = /[a-z]/.test(password);
+        this.passwordHasNumber = /[0-9]/.test(password);
+        this.passwordHasSpecialChar = /[\W_]/.test(password);
+        this.passwordHasMinLength = /.{8,}/.test(password);
+
+        const complexityRules = [
+            { regex: /[A-Z]/, message: 'At least one uppercase letter' },
+            { regex: /[a-z]/, message: 'At least one lowercase letter' },
+            { regex: /[0-9]/, message: 'At least one number' },
+            { regex: /[\W_]/, message: 'At least one special character' },
+            { regex: /.{8,}/, message: 'At least 8 characters long' }
+        ];
+
+        const failedRules = complexityRules.filter(rule => !rule.regex.test(password));
+        this.passwordComplexityMessage = failedRules.length === 0 ? 'Password is strong' : 'Password must contain: ' + failedRules.map(rule => rule.message).join(', ');
+    }
 
     register() {
-        this.authService.register(this.user).subscribe(response => {
-            if (response.success) {
-                this.router.navigate(['/login']);
-            } else {
-                alert('Registration failed');
-            }
+        if (this.user.password !== this.user.confirmPassword) {
+            alert('Passwords do not match');
+            return;
+        }
+
+        this.reCaptchaV3Service.execute('register', 'action_name', (token: string) => {
+            this.handleSuccess(token);
+            this.authService.register({ ...this.user, captchaResponse: this.captchaResponse }).subscribe(response => {
+                if (response.success) {
+                    this.router.navigate(['/login']);
+                } else {
+                    alert('Registration failed');
+                }
+            });
         });
     }
 }
