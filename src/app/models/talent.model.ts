f2c19c5a0c8ed5252a96b9c85fe74fbece8425da
Guido Salzano <stukutra@gmail.com>
2025-03-03 12:20:40 +0100

    update

--- src/app/models/talent.model.ts
+++ src/app/models/talent.model.ts
@@ -2,19 +2,19 @@ export interface Talent {
     role: string;
     minContract: string;
     seniority: string;
-    dailyRate: string; // Cambiato da number a string
-    pricePerDay: string; // Cambiato da number a string
+    dailyRate: string;
+    pricePerDay: string; 
     skills: string[];
     frameworks_libraries: string[];
     tools: string[];
     languages_spoken: {
         [language: string]: string;
     };
-    expanded?: boolean; // Aggiungi questa proprietà
-    expandedSkills?: boolean; // Aggiungi questa proprietà
-    expandedFrameworks?: boolean; // Aggiungi questa proprietà
-    expandedTools?: boolean; // Aggiungi questa proprietà
-    expandedLanguages?: boolean; // Aggiungi questa proprietà
+    expanded?: boolean; 
+    expandedSkills?: boolean; 
+    expandedFrameworks?: boolean; 
+    expandedTools?: boolean; 
+    expandedLanguages?: boolean; 
 }
 
 export interface Category {
