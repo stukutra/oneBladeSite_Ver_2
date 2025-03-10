558bc9ceb1836cd0de9d3e8c9cae84aeba3795b3
Guido Salzano <stukutra@gmail.com>
2025-03-10 19:22:14 +0100

    update blog

---
+++ generate-sitemap.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const urls = JSON.parse(fs.readFileSync(path.join(__dirname, 'blog-urls.json'), 'utf-8'));
+
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.map(url => `
+    <url>
+        <loc>${url.loc}</loc>
+        <lastmod>${url.lastmod}</lastmod>
+        <changefreq>${url.changefreq}</changefreq>
+        <priority>${url.priority}</priority>
+    </url>`).join('')}
+</urlset>`;
+
+fs.writeFileSync(path.join(__dirname, 'sitemap.xml'), sitemap);
+console.log('Sitemap generated successfully.');
