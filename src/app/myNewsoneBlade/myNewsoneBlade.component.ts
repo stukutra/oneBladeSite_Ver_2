5ecba858270e3ed9d575e467112b7f90c45383e5
Stukutra <stukutra@gmail.com>
2025-03-24 22:46:04 +0100

    Update News

--- src/app/myNewsoneBlade/myNewsoneBlade.component.ts
+++ src/app/myNewsoneBlade/myNewsoneBlade.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 import { News } from '../models/news.model';
 import { NewsService } from '../service/news.service';
@@ -11,14 +11,31 @@ import { NewsService } from '../service/news.service';
 export class MyNewsoneBladeComponent implements OnInit {
   news: News[] = [];
   currentPage: number = 1;
-  newsPerPage: number = 3;
+  newsPerPage: number = 3; // Default to 3 items per page
 
   constructor(private newsService: NewsService) { }
 
   ngOnInit() {
+    this.updateNewsPerPage(); // Set the initial number of items per page
     this.newsService.getNews().subscribe((data) => {
       this.news = data.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
     });
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.updateNewsPerPage(); // Update the number of items per page on window resize
+  }
+
+  private updateNewsPerPage() {
+    const width = window.innerWidth;
+    if (width < 576) {
+      this.newsPerPage = 1; // 1 item for small screens
+    } else if (width < 768) {
+      this.newsPerPage = 2; // 2 items for medium screens
+    } else {
+      this.newsPerPage = 3; // 3 items for large screens
+    }
   }
 
   get paginatedNews(): News[] {
