e8cb8334b10f1c1c06f4834f9cd2c6df15673e3d
Guido Salzano <stukutra@gmail.com>
2025-04-01 20:47:53 +0200

    update

--- src/app/pages/image-carousel/image-carousel.component.ts
+++ src/app/pages/image-carousel/image-carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 
 import { CoursesService } from '../../service/Courses.service';
 
@@ -15,23 +15,32 @@ interface Slide {
   templateUrl: './image-carousel.component.html',
   styleUrls: ['./image-carousel.component.scss']
 })
-export class ImageCarouselComponent implements OnInit {
+export class ImageCarouselComponent implements OnInit, OnDestroy {
   currentSlide = 0;
+  private carouselInterval: any; // Store the interval reference
   slides: Slide[] = [
     { title: 'CAROUSEL.SLIDE_1.TITLE', subtitle: 'CAROUSEL.SLIDE_1.SUBTITLE', image: './assets/slide/Slide_1.jpg', alt: 'Description of Slide 1' },
     { title: 'CAROUSEL.SLIDE_2.TITLE', subtitle: 'CAROUSEL.SLIDE_2.SUBTITLE', image: './assets/slide/Slide_2.jpg', alt: 'Description of Slide 2' },
     { title: 'CAROUSEL.SLIDE_3.TITLE', subtitle: 'CAROUSEL.SLIDE_3.SUBTITLE', image: './assets/slide/Slide_1.jpg', alt: 'Description of Slide 3' }
   ];
 
-  constructor(private coursesService: CoursesService) { }
+  constructor(private coursesService: CoursesService, private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
-    this.startCarousel();
+    this.currentSlide = 0; // Ensure the first slide is active immediately
+    this.cdr.detectChanges(); // Force Angular to detect changes and render the first slide
+    this.startCarousel(); // Start the carousel immediately without delay
     this.loadCourseSlides();
   }
 
+  ngOnDestroy() {
+    if (this.carouselInterval) {
+      clearInterval(this.carouselInterval); // Stop the carousel when the component is destroyed
+    }
+  }
+
   startCarousel() {
-    setInterval(() => {
+    this.carouselInterval = setInterval(() => {
       this.currentSlide = (this.currentSlide + 1) % this.slides.length;
     }, 3000); // Change slide every 3 seconds
   }
