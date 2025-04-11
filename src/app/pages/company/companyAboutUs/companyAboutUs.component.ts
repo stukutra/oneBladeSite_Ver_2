3081ed1287303d05c145b61e27ee54176a872109
Guido Salzano <stukutra@gmail.com>
2025-04-11 13:40:04 +0200

    Update stack

--- src/app/pages/company/companyAboutUs/companyAboutUs.component.ts
+++ src/app/pages/company/companyAboutUs/companyAboutUs.component.ts
@@ -8,40 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CompanyAboutUsComponent implements OnInit {
 
   constructor() { }
-  dataGroups = [
-    {
-      category: 'Sviluppo Web',
-      technologies: [
-        { name: 'Fullstack', value: 95 },
-        { name: 'Angular', value: 90 },
-        { name: 'React', value: 60 }
-      ]
-    },
-    {
-      category: 'Mobile Development',
-      technologies: [
-        { name: 'Mobile (iOS, React Native, Android)', value: 80 },
-        { name: 'IOS', value: 60 },
-        { name: 'React Native', value: 70 }
-      ]
-    },
-    {
-      category: 'Backend & Cloud',
-      technologies: [
-        { name: 'DOT NET', value: 60 },
-        { name: 'Python', value: 40 },
-        { name: 'Data Engineering', value: 60 }
-      ]
-    },
-    {
-      category: 'Quality Assurance & UI/UX',
-      technologies: [
-        { name: 'QA', value: 40 },
-        { name: 'UI/UX', value: 75 }
-      ]
-    }
-  ];
-
+ 
   ngOnInit() {
   }
 
