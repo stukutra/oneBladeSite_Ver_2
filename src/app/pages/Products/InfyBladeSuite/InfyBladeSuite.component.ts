e5c8150172ed6ad36e2014b1d0457e3696d5e7c4
Guido Salzano <stukutra@gmail.com>
2025-04-14 18:31:43 +0200

    Update

--- src/app/pages/Products/InfyBladeSuite/InfyBladeSuite.component.ts
+++ src/app/pages/Products/InfyBladeSuite/InfyBladeSuite.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-InfyBladeSuite',
   templateUrl: './InfyBladeSuite.component.html',
-  styleUrls: ['./InfyBladeSuite.component.scss']
+  styleUrls: ['./InfyBladeSuite.component.scss'] // Ensure this path is correct
 })
 export class InfyBladeSuiteComponent implements OnInit {
 
